bf45e7c70b9c1243c2cf00e925db53a94f542224
Kerber <dustin.kerber@gmail.com>
2019-03-17 17:51:00 -0500

    Create zapper.ino

---
+++ zapper.ino
@@ -0,0 +1,115 @@
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+
+// Network name for Wifi Access Point
+#define WIFI_SSID "Wizards Rule"
+
+// Milliseconds to keep relay open
+#define ZAP_TIME 4000
+
+// Misc Setup
+const byte DNS_PORT = 53;
+IPAddress apIP(192, 168, 1, 1);
+DNSServer dnsServer;
+ESP8266WebServer server(80);
+
+// Misc variables for keeping track of things
+unsigned long lastZapStartTime;
+boolean zapping = false;
+
+// Webserver response to http://192.168.0.1/
+// Displays a simple page for user interaction
+void handleRoot() {
+  Serial.println("Enter handleRoot");
+  // Super simple website for user interaction
+  // Show a button that hits a URL
+  String CSS
+  String content = "<html><body style=\"background-color:black;\"><div style=\"width:100%;height:100%;display:flex;align-items:center;justify-content:center;\">"
+                   "<button class=\"button\" style=\"height:400px;width:400px;background-color:red;color:white;font-size:4em;border-radius:50%;\" onclick=\"send()\">Zap</button>"
+                   "<script>function send(){ var xhttp = new XMLHttpRequest();"
+                   "xhttp.open(\"GET\", \"zap\", true);"
+                   "xhttp.send();}</script>"
+                   "</body></html>";
+  server.send(200, "text/html", content);
+}
+
+// Webserver response to http://192.168.0.1/zap
+// Turns on the zapper
+void handleZap() {
+  Serial.println("Enter handleRoot");
+  Serial.println("starting zap");
+  // Set the time for zapper auto-off
+  lastZapStartTime = millis();
+  // Activate relay
+  zapping = true;
+  setRelayState(zapping);
+  // Redirect the user to the default landing page
+  server.sendHeader("Location", "/");
+  server.sendHeader("Cache-Control", "no-cache");
+  server.send(301);
+}
+
+// Run constantly to turn of the zapper when the time has expired
+void checkZapTimer() {
+  // If currently zapping
+  if (zapping) {
+    // Check if they've had enough
+    if (millis() - lastZapStartTime > ZAP_TIME) {
+      // Disable relay
+      zapping = false;
+      setRelayState(zapping);
+      Serial.println("ending zap");
+    }
+  }
+}
+
+// Hide the confusing fact that you have to set the LED_BUILTING pin to LOW to turn on the zapper
+// Ain't noone got time for that
+void setRelayState(bool zap) {
+  digitalWrite(LED_BUILTIN, !zap);
+}
+
+// Initial Arduino setup
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting Sketch...");
+
+  // Initialize the LED_BUILTIN pin as an output
+  // It just happens to also be connected to the relay for easy debugging
+  pinMode(LED_BUILTIN, OUTPUT);
+  // Initialize the relay to the predetermined start state
+  setRelayState(zapping);
+
+  // Set up the Wifi to be an Access Point
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  // Start the access point with the predetermined SSID
+  WiFi.softAP(WIFI_SSID);
+
+  Serial.println("Starting captive webserver.");
+
+  // If DNSServer is started with "*" for domain name, it will reply with
+  // this device's IP to all DNS request. This makes it a 'captive portal'.
+  // Most smartphones will automatically direct the user to this page when connected.
+  dnsServer.start(DNS_PORT, "*", apIP);
+
+  // This is the default endpoint returned when the user is directed to our IP
+  server.on("/", HTTP_GET, handleRoot);
+  // This is the endpoint that activates the zapper
+  server.on("/zap", HTTP_GET, handleZap);
+  // Reply to any unknown requests to our default page
+  server.onNotFound(handleRoot);
+  server.begin();
+
+}
+
+// Arduino main loop
+void loop() {
+  // Handle any waiting DNS requests
+  dnsServer.processNextRequest();
+  // Handle any URL requests
+  server.handleClient();
+  // Check whether zapper needs to be turned off
+  checkZapTimer();
+}
